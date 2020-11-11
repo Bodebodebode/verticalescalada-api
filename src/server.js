267c72db36d85fe207805823ed6022c7ce2af134
bodebodebode <bode_slash@hotmail.com>
2020-11-11 20:26:57 -0300

    so roda meu fi

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors({ origin: "http://localhost:3000", credentials: true }))
-app.use(session({ secret: "secretcode", resave: true }));
+app.use(session({ secret: "secretcode", resave: true , saveUninitialized: true }));
 app.use(cookieParser("secretcode"));
 app.use(passport.initialize());
 app.use(passport.session());
